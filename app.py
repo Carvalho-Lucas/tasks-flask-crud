d3fc19b38ca98dada01dd84ceb8adb509ce6a49a
Carvalho-Lucas <lukaspc2019@gmail.com>
2025-04-09 15:39:39 -0300

    Update method POST in app.py / Add tests.py -> Create test for POST method in app.py

--- app.py
+++ app.py
@@ -13,18 +13,19 @@ task_id_control = 1
 @app.route("/tasks", methods=['POST'])
 def create_task():
     global task_id_control
-    data = request.get_json()# pega o JSON enviado pelo cliente
+    data = request.get_json()  # Recebe os dados enviados pelo cliente
 
-    title = data.get("title")# extrai o título
-    description = data.get("description", "")
+    new_task = Task(
+        id=task_id_control,
+        title=data['title'],
+        description=data.get("description", "")
+    )
+    
+    task_id_control += 1  # Atualiza o ID para a próxima tarefa
+    tasks.append(new_task)  # Salva a nova tarefa
 
-    if not title:
-        return jsonify({"error": "Título é obrigatório"}), 400
-
-    new_task = Task(id=task_id_control, title=title, description=description)# cria tarefa
-    task_id_control += 1
-    tasks.append(new_task)
-    return jsonify(new_task.to_dict()), 201  # Retorna a nova tarefa criada + status 201
+    print(tasks)  # DEBUG: mostra no console
+    return jsonify({"message": "Nova tarefa criada com sucesso", "id": new_task.id})# Retorna a nova tarefa criada + status 200
 
 # ROTA GET - LISTAR TODAS as tarefas
 @app.route('/tasks', methods=['GET'])
