d80efe878f9535bf7735dc5cd9208bfe51378635
Carvalho-Lucas <lukaspc2019@gmail.com>
2025-04-09 16:35:53 -0300

    Update task.py -> Completed. Adding tests & Updating create endpoint

--- models/task.py
+++ models/task.py
@@ -1,17 +1,17 @@
 class Task:
 
-    def __init__(self, id, title, description, completad=False):
+    def __init__(self, id, title, description, completed=False):
         self.id = id
         self.title = title
         self.description = description
-        self.completad = completad
+        self.completed = completed
         
     def to_dict(self):
         return{
             "id": self.id,
             "title": self.title,
             "description": self.description,
-            "completad": self.completad,
+            "completed": self.completed,
         }
     
     
