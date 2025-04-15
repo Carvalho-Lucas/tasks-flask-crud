f61d92b80d8714b57be757df747fe5881ea0af71
Carvalho-Lucas <lukaspc2019@gmail.com>
2025-04-15 15:14:32 -0300

    Update route create_task()

--- app.py
+++ app.py
@@ -7,25 +7,36 @@ app = Flask(__name__)
 tasks = []
 
 # Controlador de ID único para cada tarefa criada
-task_id_control = 1
+proximo_id = 1
 
 # ROTA POST - CRIAR uma nova tarefa
 @app.route("/tasks", methods=['POST'])
 def create_task():
-    global task_id_control
+    global proximo_id
     data = request.get_json()  # Recebe os dados enviados pelo cliente
 
-    new_task = Task(
-        id=task_id_control,
-        title=data['title'],
-        description=data.get("description", "")
-    )
-    
-    task_id_control += 1  # Atualiza o ID para a próxima tarefa
-    tasks.append(new_task)  # Salva a nova tarefa
+    title = data.get("title")
+    description = data.get("description")
 
-    print(tasks)  # DEBUG: mostra no console
-    return jsonify({"message": "Nova tarefa criada com sucesso", "id": new_task.id})# Retorna a nova tarefa criada + status 200
+    # Validação: title obrigatório, description opcional mas não vazio
+    if not title:
+        return jsonify({"ERROR": "Título é obrigatório"}), 400
+    elif not description:
+        return jsonify({"ERROR": "Descrição é obrigatória"}), 400
+
+    new_task = Task(
+        id=proximo_id,
+        title=title,
+        description=description
+    )
+
+    proximo_id += 1
+    tasks.append(new_task)
+
+    return jsonify({
+        "message": "Nova tarefa criada com sucesso",
+        "id": new_task.id
+    }), 201
 
 # ROTA GET - LISTAR TODAS as tarefas
 @app.route('/tasks', methods=['GET'])
